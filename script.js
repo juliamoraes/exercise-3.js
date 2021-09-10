6bb0a7b00c56b5a2bedda57c0299f91330b59e85
juliamoraes <68563285+juliamoraes@users.noreply.github.com>
2021-09-10 20:21:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let salary = window.prompt(`How much do you make per month?`); //getting variable `salary` 
+let salary = parseFloat(window.prompt(`How much do you make per month?`)); //getting variable `salary` 
 const annual = 12;
 
 document.write(`Your monthly salary is ${salary.toLocaleString (`en-GB` , {style: `currency` , currency: `GBP` })}\n`); //Returns a date converted to a string using the current locale.
